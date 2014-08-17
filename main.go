00784c47a0aea55a0730b84d97ea6e5d47558445
shenyubao <ssybb1988@gmail.com>
2014-08-17 14:43:57 +0800

    init beego

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "souche-public-service/docs"
+	_ "souche-public-service/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
